76607913ef82e4a14e10a9d65234f9a55255e1e6
Loni Bower <mountain.princess@rocketmail.com>
2020-02-07 13:12:53 -0800

    darrin test

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://mountainprincess:mountain.princess@1980@churchusersprayerrequests-92bir.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => console.log('connected to db'));
+mongoose.connect('mongodb+srv://me:me2@churchpeeps3-ilnbs.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => console.log('connected to db'));
 
 const app = express();
 const router = express.Router();
+
+console.log("darrin test");
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +18,7 @@ const usersRouter = require('./routes/users');
 
 app.use('/prayer', PrayerRouter);
 app.use('/users', usersRouter);
+// app.use('/users/admin', adminRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
